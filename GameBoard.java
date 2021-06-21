7bcf2b142ec3df9ad9fa22a42bdd8a682a756add
LaurenKScott <lkscottcom@gmail.com>
2021-06-21 17:16:49 -0400

    Add food drawing methods to game board

--- GameBoard.java
+++ GameBoard.java
@@ -1,4 +1,4 @@
-//import java.util.Random;
+import java.util.Random;
 import javax.swing.JPanel;
 import java.awt.*;
 //import java.awt.event.*;
@@ -7,20 +7,32 @@ public class GameBoard extends JPanel{
 
     static final int BOARD_HEIGHT = 600;
     static final int BOARD_WIDTH = 600;
-    static final int CELL_SIZE = 25;
+    static final int CELL_SIZE = 20;
     static final int CELL_COUNT = ((BOARD_HEIGHT * BOARD_WIDTH) / CELL_SIZE);
+
+    final int x[] = new int[CELL_COUNT];
+    final int y[] = new int[CELL_COUNT];
+    int foodX;
+    int foodY;
+    int snakeSize = 1;
     boolean running = false;
+    Random random;
 
     GameBoard(){
+        random = new Random();
         this.setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
         this.setBackground(Color.DARK_GRAY);
         this.setFocusable(true);
         //this.addKeyListener();
         startGame();
     }
-
     public void startGame(){
+        newFood();
         running = true;
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
     }
     public void draw(Graphics g){
         for (int i=0; i<(BOARD_WIDTH/CELL_SIZE);i++) {
@@ -28,6 +40,13 @@ public class GameBoard extends JPanel{
             g.drawLine(i*CELL_SIZE, 0, i*CELL_SIZE, BOARD_HEIGHT);
             g.drawLine(0, i*CELL_SIZE, BOARD_WIDTH, i*CELL_SIZE);
         }
+        g.setColor(Color.red);
+        g.fillOval(foodX, foodY, CELL_SIZE, CELL_SIZE);
+    }
+    public void newFood() {
+        //set random x and y coordinates for next food location
+        foodX = random.nextInt((int)(BOARD_WIDTH/CELL_SIZE))*CELL_SIZE;
+        foodY = random.nextInt((int)(BOARD_HEIGHT/CELL_SIZE))*CELL_SIZE;
     }
     
 }
--- SnakeGame.java
+++ SnakeGame.java
@@ -7,7 +7,7 @@
 
 public class SnakeGame {
     public static void main(String[] args){
-        new GameBoard();
+        new GameFrame();
     }
    
 }
