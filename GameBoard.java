de8fb223591f1811cd948f7eeb25b2170f3a3ba0
LaurenKScott <lkscottcom@gmail.com>
2021-06-21 19:48:08 -0400

    Update checkCollisions method with coordinates

--- GameBoard.java
+++ GameBoard.java
@@ -3,15 +3,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class GameBoard extends JPanel{
+public class GameBoard extends JPanel implements ActionListener{
 
     static final int BOARD_HEIGHT = 600;
     static final int BOARD_WIDTH = 600;
     static final int CELL_SIZE = 20;
     static final int CELL_COUNT = ((BOARD_HEIGHT * BOARD_WIDTH) / CELL_SIZE);
-    static final int DELAY = 100;
-    final int x[] = new int[CELL_COUNT];
-    final int y[] = new int[CELL_COUNT];
+    static final int SPEED = 100;
+    //represents x, y coordinates of snake, with the head at [0][0]
+    final int snakeX[] = new int[CELL_COUNT];
+    final int snakeY[] = new int[CELL_COUNT];
 
     int foodX;
     int foodY;
@@ -32,7 +33,8 @@ public class GameBoard extends JPanel{
     public void startGame(){
         newFood();
         running = true;
-        timer = new Timer(DELAY, this);
+        timer = new Timer(SPEED,this);
+        timer.start();
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
@@ -53,11 +55,11 @@ public class GameBoard extends JPanel{
             if (i == 0) {
                 // set head color to different shade of green for improved visibility
                 g.setColor(new Color(50, 200, 0));
-                g.fillRect(x[i], y[i], CELL_SIZE, CELL_SIZE);
+                g.fillRect(snakeX[i], snakeY[i], CELL_SIZE, CELL_SIZE);
             }
             else {
                 g.setColor(Color.GREEN);
-                g.fillRect(x[i], y[i], CELL_SIZE, CELL_SIZE);
+                g.fillRect(snakeX[i], snakeY[i], CELL_SIZE, CELL_SIZE);
             }
         }
     }
@@ -69,23 +71,23 @@ public class GameBoard extends JPanel{
     public void moveSnake(){
         //Change coordinates of the snake moving from HEAD to TAIL
         for (int i=snakeSize; i>0; i--) {
-            x[i] = x[i-1];
-            y[i] = y[i-1];
+            snakeX[i] = snakeX[i-1];
+            snakeY[i] = snakeY[i-1];
         }
         switch(direction) {
             case 'U':
                 //top is at  y=0. shift y coords UP
-                y[0] = y[0] - CELL_SIZE;
+                snakeY[0] = snakeY[0] - CELL_SIZE;
                 break;
             case 'D':
-                y[0] = y[0] + CELL_SIZE;
+                snakeY[0] = snakeY[0] + CELL_SIZE;
                 break;
             case 'L':
                 //left is at x=0. shift x coords LEFT
-                x[0] = x[0] - CELL_SIZE;
+                snakeX[0] = snakeX[0] - CELL_SIZE;
                 break;
             case 'R': 
-                x[0] = x[0] + CELL_SIZE;
+                snakeX[0] = snakeX[0] + CELL_SIZE;
                 break;
         }
     }
@@ -93,12 +95,28 @@ public class GameBoard extends JPanel{
 
     }
     public void checkCollisions() {
-
+        //iterate over snake body. if collision occurs, set running to false
+        for (int i=snakeSize; i>0; i--) {
+            if ((snakeX[0] == snakeX[i]) && (snakeY[0] == snakeY[i])) {
+                running = false;
+            }
+        }
+        //check for snake-border collisions by x coordinate
+        if ((snakeX[0] < 0) || (snakeX[0] > BOARD_WIDTH)) {
+            running = false;
+        }
+        //check for snake-border collisions by y coordinate
+        if ((snakeY[0] < 0) || (snakeY[0] > BOARD_HEIGHT)) {
+            running = false;
+        }
+        if (!running) {
+            timer.stop();
+        }
     }
     public void gameOver(Graphics g) {
 
     }
-    
+    @Override
     public void actionPerformed(ActionEvent e){
         if (running) {
             moveSnake();
@@ -107,5 +125,7 @@ public class GameBoard extends JPanel{
         }
         repaint();
     }
-    public class gameKeyAdapter extends KeyAdapter {}
+    public class gameKeyAdapter extends KeyAdapter {
+
+    }
 }
